6973b6e85c0ddcbe70182aca1b7275db34c8e77a
Kanaan <krs8dd@umsystem.edu>
2024-02-22 14:57:17 -0600

    Testing out clock feature

---
+++ oss.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <string.h>
+#include <time.h>
+#include <signal.h>
+
+
+// Set up the PCB according to what was given in the instructions
+struct PCB {
+    int occupied;
+    pid_t pid;
+    int startSeconds;
+    int startNano;
+};
+
+// Shared memory Key
+const int sh_key 205569;
+
+// Variables
+int* sysClock;
+int proc, simul;
+int clockSmh;
+char* timeLimit;
+
+static void myHandler(int s) {
+    printf("Got signal %d, terminate!\n" , s);
+    exit(1);
+}
+
+static int setupinterrupt (void) {
+    struct sigaction act;
+    act.sa handler = myHandler();
+    act.sa flags = 0;
+
+    return(sigemptyset(&act.sa_mask) || sigaction(SIGINT , &act, NULL) || sigaction(SIGPROF , &act , NULL));
+}
+
+static int setupitimer(void) { /* set ITIMER_PROF for 2-second intervals */
+    struct itimerval value;
+    value.it_interval.tv_sec = 60;
+    value.it_interval.tv_usec = 0;
+    value.it_value = value.it_interval;
+    return (setitimer(ITIMER_PROF, &value, NULL));
+}
+
+
+void print_usage(const char *progName) {
+    printf("Usage for %s: -n <n_value> -s <s_value> -t <t_value> -i <i_value>\n" , progName);
+    printf("Options:\n");
+    printf("-n: stands for the total number of workers to launch\n");
+    printf("-s: Defines how many workers are allowed to run simultaneously\n");
+    printf("-t: The time limit to pass to the workers\n");
+    printf("-i: How often a worker should be launched")
+}
+
+int main(int argc, char *argv[]) {
+    if (setupinterrupt() == -1) {
+        perror("Failed to set up handler for SIGPROF");
+        return 1;
+    }
+    if (setupitimer() == -1) {
+        perror("Failed to set up ITIMER_PROF interval timer");
+        return 1;
+    }
+    for ( ; ; );
+}
